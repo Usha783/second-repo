fcad722620dd9e61966e3c3d72d0f556fe735b38
Usha Padigeri <padigeri@achala.local>
2025-07-17 16:52:57 +0530

    changes in code2

--- code2.java
+++ code2.java
@@ -1,6 +1,6 @@
 public class code2 {
     public static void main(String[] args){
         System.out.println("Hi");
-        System.out.println("This is");
+        System.out.println("This is Usha");
     }
 }
